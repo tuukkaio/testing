7edbb9bc2d98bd339544578c63d1448abfd5387a
tuukkaio <128314510+tuukkaio@users.noreply.github.com>
2023-03-27 14:48:05 -0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
-console.log("This is a popup!")
+// Inject the payload.js script into the current tab after the popout has loaded
+window.addEventListener('load', function (evt) {
+	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+		file: 'payload.js'
+	});;
+});
+
+// Listen to messages from the payload.js script and write to popout.html
+chrome.runtime.onMessage.addListener(function (message) {
+	document.getElementById('pagetitle').innerHTML = message;
+});
